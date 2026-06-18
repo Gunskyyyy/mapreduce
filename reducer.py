f1b13db7160eb8ca047b22ac1650b646776d2829
Gunskyyyy <christopher.vincent2002@gmail.com>
2026-06-18 14:09:52 +0200

    Exercise 6: average sales per category

--- reducer.py
+++ reducer.py
@@ -3,6 +3,7 @@
 import sys
 
 count = 0
+total = 0
 previous_key = None
 
 for line in sys.stdin:
@@ -11,12 +12,14 @@ for line in sys.stdin:
     key, value = data
 
     if previous_key != None and previous_key != key:
-        if count > 114:
-            sys.stdout.write("{0}\t{1}\n".format(previous_key, count))
+        average = total / count
+        sys.stdout.write("{0}\t{1}\n".format(previous_key, round(average, 2)))
         count = 0
+        total = 0
 
     count += 1
+    total += float(value)
     previous_key = key
 
-if count > 114:
-    sys.stdout.write("{0}\t{1}\n".format(previous_key, count))
+average = total / count
+sys.stdout.write("{0}\t{1}\n".format(previous_key, round(average, 2)))
